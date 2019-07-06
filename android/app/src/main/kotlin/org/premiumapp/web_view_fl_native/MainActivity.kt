50c32fd6a086f362eabeceb34636bb7c1d46787c
a <ruan65@gmail.com>
2019-07-07 01:50:31 +0300

    initial

---
+++ android/app/src/main/kotlin/org/premiumapp/web_view_fl_native/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.premiumapp.web_view_fl_native
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
